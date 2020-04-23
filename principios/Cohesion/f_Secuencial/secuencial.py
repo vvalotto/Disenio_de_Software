57b21c4b00fd0bc2d7e1876dbfedd407281d9ac5
Victor Valotto <vvalotto@gmail.com>
2020-04-23 09:19:03 -0300

    Armado básico completo para Cohesión

--- principios/Cohesion/f_Secuencial/secuencial.py
+++ principios/Cohesion/f_Secuencial/secuencial.py
@@ -1,24 +1,22 @@
 '''
 
-class adquisidor(object):
+class adquisicion(object):
 
     def adquirir(self):
         return senial
 
 
-class filtro(object):
+class filtrado(object):
 
     def filtrar(self, senial):
         return senial
-
 
 class proceso(object):
 
     def procesar(self, senial):
         return senial
 
-
-class almacenador(object):
+class guardado(object):
 
     def guardar(self, senial):
         return senial
@@ -27,10 +25,10 @@ class almacenador(object):
 class controlador(object):
 
     def __init__(self):
-        self._a = adquisidor()
-        self._f = filtro()
+        self._a = adquisicion()
+        self._f = filtrado()
         self._p = proceso()
-        self._g = almacenador()
+        self._g = guardado()
 
     def ejecutar(self):
 
