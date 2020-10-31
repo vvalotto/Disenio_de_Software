eb0d7fe51e758f35d5258075ddeab99131ffb1c5
Victor Valotto <vvalotto@gmail.com>
2020-10-31 15:39:33 -0300

    Creación de las primeras entidades

---
+++ patrones/grasp/entidades/bioquimico.py
@@ -0,0 +1,10 @@
+"""
+Clase Medico
+"""
+from .persona import *
+
+
+class Bioquimico(Persona):
+
+    def __init__(self):
+        super().__init__()
---
+++ patrones/grasp/entidades/medico.py
@@ -0,0 +1,10 @@
+"""
+Clase Medico
+"""
+from .persona import *
+
+
+class Medico(Persona):
+
+    def __init__(self):
+        super().__init__()
