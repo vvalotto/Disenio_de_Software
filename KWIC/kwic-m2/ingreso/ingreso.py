39d997b477bc72a57fc887ec847eccb68e39b55d
Victor Valotto <vvalotto@gmail.com>
2020-12-14 12:04:27 -0300

    Commit Inicial - Posteriormente se debe armar con pasos evolutivos

---
+++ KWIC/kwic-m2/ingreso/ingreso.py
@@ -0,0 +1,19 @@
+
+from palabras.palabras import *
+
+
+def leer_linea_texto(nombre_archivo):
+    lineas = Lineas()
+    try:
+        archivo = open(nombre_archivo, 'r')
+        lineas_leidas = archivo.readlines()
+        archivo.close()
+    except IOError:
+        print("Error en la lectura del archivo")
+    else:
+        for linea in lineas_leidas:
+            palabras = Palabras()
+            for palabra in linea.split():
+                palabras.agregar_palabra(palabra)
+            lineas.agregar_linea(palabras)
+    return lineas
